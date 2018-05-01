bed8186367aee66b82863273eef36ed63718a733
BiancaArtola <bian.artola@hotmail.com>
2018-05-01 11:52:25 -0300

    Creacion models

---
+++ project/app_server/models/recorridos.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+const esquemaPuntos = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true
+  },
+  direccion: {
+    type: String,
+    required: true
+  },
+  imagen: {
+  	type: String,
+    required: true
+  },
+  coordenadas: {
+  	type: [Number],
+  	required: true
+  }
+
+})
+
+
+const esquemaRecorridos = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true
+  },
+  tiempo: {
+    type: Number,
+    required: true
+  },
+  categoria: {
+    type: String,
+    required: true
+  },  
+  tarifa: {
+  	type: Number,
+    required: true
+  },
+  descripcion: {
+    type: String,
+    required: true
+  },
+  apto: {
+    type: [String],
+    required: true
+  },
+  puntos: {
+  	type: [esquemaPuntos],
+  	required: true
+  }
+
+});
+
+mongoose.model('Recorridos', esquemaRecorridos);
