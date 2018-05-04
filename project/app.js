420d5655149ee24492953450f725a10433fa9d39
Agustin Streitenberger <a_streiten@outlook.com>
2018-05-04 19:38:58 -0300

    545

--- project/app.js
+++ project/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-require('./app_server/models/Recorridos')
+
 require('./app_server/models/db')
 
 
