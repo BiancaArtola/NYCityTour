b6c4e6df20d35514798a91b2d1b23db41442a8b6
Agustin Streitenberger <a_streiten@outlook.com>
2018-05-04 19:20:50 -0300

    Intento fix

--- project/app.js
+++ project/app.js
@@ -4,7 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('./app_server/models/Recorridos')
 require('./app_server/models/db')
+
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
