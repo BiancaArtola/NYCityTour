92c0f91928f3bfa0b92fc2e40841169bf53deedf
Agustin Streitenberger <a_streiten@outlook.com>
2018-05-04 19:44:29 -0300

    ghj

--- project/app_server/models/db.js
+++ project/app_server/models/db.js
@@ -3,4 +3,4 @@ const mongoose = require('mongoose');
 const dbURI = 'mongodb://localhost:27017/Recorridos';
 mongoose.connect(dbURI);
 
-require('./Recorridos');
+require('./app_server/models/Recorridos');
