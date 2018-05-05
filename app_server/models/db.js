cf0c472a5663fc1643414442936639992f674698
Agustin Streitenberger <a_streiten@outlook.com>
2018-05-05 18:28:56 -0300

    new uri

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 const dbURI = 'mongodb://localhost:27017/Recorridos';
-mongoose.connect(dbURI);
-
+//mongoose.connect(dbURI);
+mongoose.connect(process.env.MONGOLAB_URI || dbURI);
 require('./recorridos');
