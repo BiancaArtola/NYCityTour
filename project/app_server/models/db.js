55ca54ade4a28fccc9b748b9d5e4c82876ce5991
BiancaArtola <bian.artola@hotmail.com>
2018-05-05 17:11:12 -0300

    Revert "ghj"
    This reverts commit 92c0f91928f3bfa0b92fc2e40841169bf53deedf.

--- project/app_server/models/db.js
+++ project/app_server/models/db.js
@@ -3,4 +3,4 @@ const mongoose = require('mongoose');
 const dbURI = 'mongodb://localhost:27017/Recorridos';
 mongoose.connect(dbURI);
 
-require('./app_server/models/Recorridos');
+require('./Recorridos');
