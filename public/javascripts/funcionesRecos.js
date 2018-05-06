dc588e3c3bc82b5a3543d632670afc72396cbb7d
Agustin Streitenberger <a_streiten@outlook.com>
2018-05-06 16:15:45 -0300

    TEST11

--- public/javascripts/funcionesRecos.js
+++ public/javascripts/funcionesRecos.js
@@ -1,12 +1,13 @@
-var apif;
-import './facebook.js';
+
 $(function() {
 
   var al=localStorage.getItem("comentario"+document.title);
   document.getElementById("paginaRecorrido").value=al;
   cargarComentarios();
   obtenerInformacionJSON();
-  alert(getStatus());
+  $.getScript("facebook.js", function() {
+   alert("Script loaded but not necessarily executed.");
+});
   
   
 
@@ -117,3 +118,45 @@ function getN(titulo)
 		}
 	}
 }
+
+$(function() {
+ 
+       window.fbAsyncInit = function() {
+        FB.init({
+        appId      : '863010233882857',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v3.0'
+       });
+       FB.AppEvents.logPageView();
+       FB.getLoginStatus(function(response) {
+         statusChangeCallback(response);
+         alert("HIJODEPUTA"+response.status);
+         });
+      }
+      });
+
+  
+      function getStatus(){
+      FB.getLoginStatus(function(response) {
+         return response;
+         });
+     }
+
+     function statusChangeCallback(response){
+     if (response.status === 'connected') {
+     var uid = response.authResponse.userID;
+     var accessToken = response.authResponse.accessToken;
+     } else if (response.status === 'not_authorized') {
+     } else { alert("no logeado"); console.log("hola");    
+     }
+     }
+
+     (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+
