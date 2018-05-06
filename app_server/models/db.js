2e5e6c108e9fa76c145582e0782c4b5d572a7832
Agustin Streitenberger <a_streiten@outlook.com>
2018-05-06 09:57:28 -0300

    remote db

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const dbURI = 'mongodb://localhost:27017/Recorridos';
-//mongoose.connect(dbURI);
-mongoose.connect(process.env.MONGOLAB_URI || dbURI);
+//const dbURI = 'mongodb://localhost:27017/Recorridos';
+const dbURI='mongodb://user:user@ds014648.mlab.com:14648/recorridos'
+mongoose.connect(dbURI);
+//mongoose.connect(process.env.MONGODB_URI || dbURI);
 require('./recorridos');
