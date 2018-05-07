442f9744f160fd0014d83a828f3c6f9ffde7400c
Agustin Streitenberger <a_streiten@outlook.com>
2018-05-07 12:32:36 -0300

    test styles

--- app_server/controllers/recorridosApi.js
+++ app_server/controllers/recorridosApi.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const recorridos = mongoose.model('recorridos');
+const estilos=mongoose.model('estilos');
 
 const getRecorridos = function (req, res) {
   recorridos
@@ -14,4 +15,16 @@ const getRecorridos = function (req, res) {
     })
 }
 
-module.exports = { getRecorridos };
+const getEstilos= function(req,res){
+	estilos
+	 .find({"user":req.params.user}).select('style')
+	  .exec((err, Recs) => {
+      if (err) { 
+        res.status(404).json(err);    
+      } else {
+        res.status(200).json(Recs);
+      }
+    })
+}
+
+module.exports = { getRecorridos, getEstilos };
--- app_server/models/db.js
+++ app_server/models/db.js
@@ -2,3 +2,4 @@ const mongoose = require('mongoose');
 const dbURI='mongodb://user:user@ds014648.mlab.com:14648/recorridos';
 mongoose.connect(dbURI);
 require('./recorridos');
+require('./estilos');
