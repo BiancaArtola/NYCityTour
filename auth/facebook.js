ab158d766ae687a4916130ed0fc8cca0984bae7b
Agustin Streitenberger <a_streiten@outlook.com>
2018-06-08 16:26:20 -0300

    nefkjhfdakfadkfad

--- auth/facebook.js
+++ auth/facebook.js
@@ -5,7 +5,7 @@ var User = require('../app_server/models/User');
 passport.use(new FacebookStrategy({
     clientID: "863010233882857",
     clientSecret: "dd7552c54381d2729ef9c03d46633628",
-    callbackURL: "http://127.0.0.1:3000/auth/facebook/callback"
+    callbackURL: "https://ciudadesturisticas.herokuapp.com/"
   },
   function(accessToken, refreshToken, profile, done) {
     User.findOrCreate({name: profile.displayName}, {name: profile.displayName,userid: profile.id}, function(err, user) {
