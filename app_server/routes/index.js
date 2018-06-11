ec0576d6d495e7f2276685eade59c0d7253d3e96
BiancaArtola <bian.artola@hotmail.com>
2018-06-11 17:21:13 -0300

    prueba pasport

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,9 +3,21 @@ var router = express.Router();
 const ctrlMain = require('../controllers/main');
 const ctrlRecorridos = require('../controllers/recorridos');
 
+var passport = require('passport');
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
 router.get('/recorridos/:id', ctrlRecorridos.reco);
+
+router.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+router.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
 
 module.exports = router;
