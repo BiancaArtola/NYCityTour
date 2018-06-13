b783d10a5ed7a90331502ebd5d1098b59ecbf07d
BiancaArtola <bian.artola@hotmail.com>
2018-06-12 21:50:14 -0300

    Cambio esquema bdd

--- app_server/models/usuarioFacebook.js
+++ app_server/models/usuarioFacebook.js
@@ -5,11 +5,9 @@ var Schema = mongoose.Schema;
 
 // Campos que vamos a guardar en la base de datos
 var UserSchema = new Schema({
-	name				: String, // Nombre del usuario
-	//provider		: String, // Cuenta del usuario (Facebook)
-	provider_id : {type: String, unique: true}, // ID que proporciona Facebook
-	photo			 : String, // Avatar o foto del usuario
-	createdAt	 : {type: Date, default: Date.now} // Fecha de creación
+		facebookID: String,
+	facebookEmail: String,
+	facebookName: String
 });
 
 // Exportamos el modelo 'User' para usarlo en otras
